7068a0c77a254c7715c02f8a373d4f52c81393de
Huan Nguyen <HuanNguyen305@gmail.com>
2020-10-30 01:21:20 -0700

    Small Fix

--- commitRemove.sh
+++ commitRemove.sh
@@ -1,2 +1,10 @@
-git commit -m "Removed all old files from github repo"
-git push origin master
+git pull origin
+git rm -r -f assets/
+git rm -f assets/
+git rm -f *.html
+git rm -f project.mobirise
+git rm -f sitemap.xml
+git add --all
+git commit -m "Cleaned Old Files"
+git push origin
+echo Repo Clean Complete
